89b1f5d2e2729ee4e8047d9341c6973bf4c7e493
ten2net <ten2net@163.com>
2023-05-27 10:56:49 +0800

    添加认证

--- routes/web.py
+++ routes/web.py
@@ -1,6 +1,9 @@
 from masonite.routes import Route
+from masonite.authentication import Auth
 
 ROUTES = [
-  Route.get("/", "WelcomeController@show"),
+  Route.get("/", "WelcomeController@show").name("welcome"),
   Route.get("/blog", "BlogController@show"),
   ]
+
+ROUTES += Auth.routes()
