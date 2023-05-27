67d268302503bc856a6fe2040b35d2dc08b3aaa9
ten2net <ten2net@163.com>
2023-05-27 11:09:39 +0800

    $ python craft migration create_posts_table --create posts

---
+++ databases/migrations/2023_05_27_110839_create_posts_table.py
@@ -0,0 +1,20 @@
+"""CreatePostsTable Migration."""
+
+from masoniteorm.migrations import Migration
+
+
+class CreatePostsTable(Migration):
+    def up(self):
+        """
+        Run the migrations.
+        """
+        with self.schema.create("posts") as table:
+            table.increments("id")
+
+            table.timestamps()
+
+    def down(self):
+        """
+        Revert the migrations.
+        """
+        self.schema.drop("posts")
